b6e188345492fd1cb1eb8540a4acf6910ded4318
eric31417 <131012569+eric31417@users.noreply.github.com>
2023-04-17 10:26:42 -0600

    Add files via upload

---
+++ Login_InfoToolsSV.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Login_InfoToolsSV
+{
+    public partial class Login_InfoToolsSV : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        string Nivel = "Nivel1";
+        protected void BtnIngresar_Click (object sender,EventArgs e)
+        {
+            string conectar = ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
+            SqlConnection sqlConectar = new SqlConnection(conectar);
+            SqlCommand cmd = new SqlCommand("Validar", sqlConectar)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            cmd.Connection.Open();
+            cmd.Parameters.Add("@Usuario", SqlDbType.VarChar, 50).Value = tbUsuario.Text;
+            cmd.Parameters.Add("@Contrasenia", SqlDbType.VarChar, 50).Value = tbPassword.Text;
+            cmd.Parameters.Add("@Nivel", SqlDbType.VarChar, 50).Value =Nivel;
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                //Agregamos una sesion de usuario
+                Session["usuariologueado"] = tbUsuario.Text;
+                Response.Redirect("Index.aspx");
+            }
+            else
+            {
+                lblError.Text = "Error de Usuario o Contrasenia";
+            }
+
+            cmd.Connection.Close();
+        }
+    }
+}
